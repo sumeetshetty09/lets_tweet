b7e4e8ad34d892c8f7d2b9b25bc3508eab597319
sumeetshetty09 <sumeetshetty09@gmail.com>
2024-12-26 00:00:03 +0530

    worked untill model registration to admin

--- lets_tweet/tweet/models.py
+++ lets_tweet/tweet/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class Tweet(models.Model):
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   text = models.TextField(max_length=255)
+   photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+   created_at = models.DateTimeField(auto_now_add=True)
+   updated_at = models.DateTimeField(auto_now=True)
+
+   def __str__(self): #it is useful for whenever we integrate this model in Admin, it will give us the demo/url from this class which can be used to see fields of the model/class which can be modified in admin page.
+     return f'{self.user.username} - {self.text[:10]}'
